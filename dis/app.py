1d9bf42e73c3a3ce65b4b4b7d694e5548fc73602
Geronyl Paragoso <31177469+fightTone@users.noreply.github.com>
2017-11-09 03:58:08 -0800

    Add files via upload

--- dis/app.py
+++ dis/app.py
@@ -1,8 +1,19 @@
+
 from login import *
 
 app = Flask(__name__)
 app.debug=True
 createdb()
+createTables()
+
+def createAdmin():
+    db.session.add(User(username="uname",passwd="safests"))
+    db.session.commit()
+
+if User.query.filter_by(username="uname").first()==None: #prevents creating another admin user
+    createAdmin()
+else:
+    pass
 
 @app.route('/')
 def login():
@@ -26,20 +37,42 @@ def u_type():
 
 @app.route('/login',methods=['POST','GET'])
 def log_in():
+
     return render_template('admLogin.html')
 
-@app.route('/verify')
+
+
+
+@app.route('/verify',methods=['GET','POST'])
 def verify():
-    if checking():
+    print  "hhhhoooowwwwwwww"
+    req=request.form['username']
+    print req
+    uname=User.query.filter_by(username=req).first()
+    print "xxxxxxx"
+    print uname
+    if check_password_hash(uname.passwd,request.form['password']):
         msg="SUCCESS"
         return render_template("admin_land.html",msg=msg)
     else:
         msg='ahahahahha youre a viewer'
         return render_template("viewer_land.html",msg=msg)
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register():
-    return render_template('adm_reg.html')
+    form=request.form
+    if request.method =='POST':
+
+        print request.form['username']
+        print request.form['password']
+        tobe_add = User( request.form['username'], request.form['password'])
+        print"im performed"
+        db.session.add(tobe_add)
+        print "im added"
+        db.session.commit()
+        msg="added succesfully"
+        return render_template('admin_land.html',msg=msg)
+    return render_template('adm_reg.html',form=form)
 
 @app.route('/proceed')
 def proceed():
--- dis/login.py
+++ dis/login.py
@@ -1,44 +1,56 @@
 import sqlalchemy
-from flask import Flask, render_template,request
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask import Flask, render_template,request,session
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import *
 from werkzeug.security import generate_password_hash, \
      check_password_hash
 
-class User(object):
-    def __init__(self, username, password):
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:4321@localhost/flaskapp123'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+
+def createdb():
+    conn = create_engine('mysql://root:4321@localhost') # connect to server
+    conn.execute("CREATE DATABASE IF NOT EXISTS flaskapp123")
+    conn.execute("USE flaskapp")
+
+def createTables():
+    db.create_all()
+
+class User(db.Model):
+    __tablename__ = "admin"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=False)
+    passwd = db.Column(db.String(200), nullable=False)
+
+    def __init__(self,username,passwd):
         self.username = username
-        #self.password=password
-        self.set_password(password)
-
-    def set_password(self, password):
-        self.pw_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.pw_hash, password)
+        self.passwd= generate_password_hash(passwd)
 
 
 
+    def set_password(self, passwd):
+        self.pw_hash = generate_password_hash(passwd)
+
+    def check_password(self, passwd):
+        return check_password_hash(self.pw_hash, passwd)
 
 
 def checking():
     User.check_password(request.form['password'])
 
 
-def createdb():
-    conn = create_engine('mysql://root:pass@localhost') # connect to server
-    conn.execute("CREATE DATABASE IF NOT EXISTS flaskapp")
-    conn.execute("USE flaskapp")
 
-    def uname_pass():
+
+    '''def uname_pass():
         user=User(request.form['username'], request.form['password'])
         cur=conn.cursor()
         cur.execute("INSERT INTO User(username,password)Values(%s,%s)",(user.username,user.password))
 
         msg = "hooooray!!"
-        return render_template('admin_land.html', msg=msg)
+        return render_template('admin_land.html', msg=msg)'''
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pass@localhost/flaskapp'
-db=SQLAlchemy(app)
